1b017ac999bb105757efa4dcd79a220879945088
ebanchik <ebanchik@gmail.com>
2023-12-05 14:51:56 -0500

    CRU_

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,15 @@ class ProductsController < ApplicationController
     @product.save
     render template: "products/show"
   end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+  
+    @product.save
+    render template: "products/show"
+  end
 end
